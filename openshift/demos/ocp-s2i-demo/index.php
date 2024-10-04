4f1731eb4a5e02124b06b2e4173aedb484d0f1dd
dmartini <dmartini@redhat.com>
2024-10-04 12:43:02 +0200

    st logo

--- openshift/demos/ocp-s2i-demo/index.php
+++ openshift/demos/ocp-s2i-demo/index.php
@@ -92,7 +92,7 @@
             <h3><?php echo "Container ID : ".gethostname(); ?></h3>
           </div>
           <div style="text-align:center">
-            <img src="images/redhat.png" />
+            <img src="images/st.png" />
           </div>
           <div style="background-color:blue;color:white;text-align:center">
             <p>Paris</p>
