cbfc7df7310860e3de2e1df4a7492c7f1095af22
dmartini <dmartini@redhat.com>
2025-01-21 14:42:09 +0100

    change size

--- openshift/demos/ocp-s2i-demo/index.php
+++ openshift/demos/ocp-s2i-demo/index.php
@@ -92,7 +92,7 @@
             <h3><?php echo "Container ID : ".gethostname(); ?></h3>
           </div>
           <div style="text-align:center">
-            <img src="images/dell.png" width="400" height="500" />
+            <img src="images/dell.png" />
           </div>
           <div style="background-color:red;color:white;text-align:center">
             <p>Paris</p>
